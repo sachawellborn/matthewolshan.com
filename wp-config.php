fb68d4e23c0d67d16e72a2c811bda3dec5ce0e20
Sacha Wellborn <sacha.wellborn@cyclinguk.org>
2025-10-01 21:18:14 +0100

    wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * The base configurations of the WordPress.
  *
@@ -15,22 +14,21 @@
  * @package WordPress
  */
 
+// ** ADD THESE LINES - Replace with your actual domain ** //
+define('WP_HOME','https://iur.cjf.mybluehost.me');
+define('WP_SITEURL','https://iur.cjf.mybluehost.me');
+
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
 define('DB_NAME', 'iurcjfmy_WPPXN');
-
 /** Database username */
 define('DB_USER', 'iurcjfmy_WPPXN');
-
 /** Database password */
 define('DB_PASSWORD', 'hP{[Kx?n]6jRO/@Z>');
-
 /** Database hostname */
 define('DB_HOST', 'localhost');
-
 /** Database charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8' );
-
 /** The database collate type. Don't change this if in doubt. */
 define( 'DB_COLLATE', '' );
 
@@ -43,15 +41,14 @@ define( 'DB_COLLATE', '' );
  *
  * @since 2.6.0
  */
-define('AUTH_KEY',         ',NXq#]DRz^/2rP-5pn8d+&/-%&&o=;cRN&)DAEKABYkeG9vOE8(I),AY/Ngs(Vgm');
-define('SECURE_AUTH_KEY',  'A+_/&?zkP]+c?!^O14t+<xu6r.|`-b{X+`*~+S4,+v.Q?!22umQFxDMzw)-2oDc+');
-define('LOGGED_IN_KEY',    '*h],3<0?Zq!-AaUQ^`5PwzC0W[YE)_0}|(T.jge-eWrm54S@SlO-U;m+vOBY>[sS');
-define('NONCE_KEY',        '<u9egQZU>T_7-}L4#IU/<0g-NZCv:GUE`E@FMo@|lk~4).uSBfPcI`q)|e@lct52');
-define('AUTH_SALT',        '5on;r-.}3d[t=^pQkmv|1N)X7bm2z-b.hI+-]_fl_jeV$9TWba-@^_S/{&4VY{d ');
+define('AUTH_KEY', ',NXq#]DRz^/2rP-5pn8d+&/-%&&o=;cRN&)DAEKABYkeG9vOE8(I),AY/Ngs(Vgm');
+define('SECURE_AUTH_KEY', 'A+_/&?zkP]+c?!^O14t+<xu6r.|`-b{X+`*~+S4,+v.Q?!22umQFxDMzw)-2oDc+');
+define('LOGGED_IN_KEY', '*h],3<0?Zq!-AaUQ^`5PwzC0W[YE)_0}|(T.jge-eWrm54S@SlO-U;m+vOBY>[sS');
+define('NONCE_KEY', '<u9egQZU>T_7-}L4#IU/<0g-NZCv:GUE`E@FMo@|lk~4).uSBfPcI`q)|e@lct52');
+define('AUTH_SALT', '5on;r-.}3d[t=^pQkmv|1N)X7bm2z-b.hI+-]_fl_jeV$9TWba-@^_S/{&4VY{d ');
 define('SECURE_AUTH_SALT', 'zV~r8F(N SBZ$0LANZkggx(M+: oZGeC=XYyNb:-$mwx4h>@WY+7-E>I|M))@T*Y');
-define('LOGGED_IN_SALT',   '2)]UKs&XN:E#h;95#U>lZPFx3w26m+@p9L4)+/A.S3_egQNG|./QF-LkX<~$n/>n');
-define('NONCE_SALT',       'z%+q(WKdv]/e  ~<Ls,pJ<j8NhIPm/n6$LGr8tx{<y-LNan?~UNL) +*#2KixC[&');
-
+define('LOGGED_IN_SALT', '2)]UKs&XN:E#h;95#U>lZPFx3w26m+@p9L4)+/A.S3_egQNG|./QF-LkX<~$n/>n');
+define('NONCE_SALT', 'z%+q(WKdv]/e ~<Ls,pJ<j8NhIPm/n6$LGr8tx{<y-LNan?~UNL) +*#2KixC[&');
 /**#@-*/
 
 /**
@@ -60,7 +57,7 @@ define('NONCE_SALT',       'z%+q(WKdv]/e  ~<Ls,pJ<j8NhIPm/n6$LGr8tx{<y-LNan?~UNL
  * You can have multiple installations in one database if you give each a unique
  * prefix. Only numbers, letters, and underscores please!
  */
-$table_prefix  = 'wp_';
+$table_prefix = 'wp_';
 
 /**
  * WordPress Localized Language, defaults to English.
@@ -84,10 +81,10 @@ define('WP_DEBUG', false);
 /* That's all, stop editing! Happy blogging. */
 
 // Include for settings managed by ddev.
-$ddev_settings = __DIR__ . '/wp-config-ddev.php';
-if ( ! defined( 'DB_USER' ) && getenv( 'IS_DDEV_PROJECT' ) == 'true' && is_readable( $ddev_settings ) ) {
-	require_once( $ddev_settings );
-}
+// $ddev_settings = __DIR__ . '/wp-config-ddev.php';
+// if ( ! defined( 'DB_USER' ) && getenv( 'IS_DDEV_PROJECT' ) == 'true' && is_readable( $ddev_settings ) ) {
+// 	require_once( $ddev_settings );
+// }
 
 /** Absolute path to the WordPress directory. */
 if ( !defined('ABSPATH') )
